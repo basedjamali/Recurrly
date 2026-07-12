0914a7f7b791c790e21ca4c32e2a96b7e49e787c
basedjamali <basedjamali@gmail.com>
2026-07-12 22:42:04 +0200

    Initial Commit

---
+++ app/(tabs)/Insides.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Insides = () => {
+  return (
+    <View>
+      <Text>Insides</Text>
+    </View>
+  );
+};
+
+export default Insides;
