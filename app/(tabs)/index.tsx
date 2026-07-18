3e1911074d36e738a859e233a42c8390c04570bd
basedjamali <basedjamali@gmail.com>
2026-07-18 22:40:25 +0200

    building the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,51 @@
 import "@/global.css";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { Image } from "expo-image";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
+import images from "@/constants/images";
+import { HOME_USER, HOME_BALANCE } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <View className="flex-1 p-5">
-        <Text className="text-5xl font-sans-extrabold">Home</Text>
+        {/* Header */}
+        <View className="home-header mb-2.5 flex-row items-center justify-between">
+          <View className="home-user flex-row items-center">
+            <Image
+              source={images.avatar}
+              className="home-avatar size-16 rounded-full"
+            />
+            <Text className="home-user-name ml-4 text-2xl font-sans-bold text-primary">
+              {HOME_USER.name}
+            </Text>
+          </View>
 
-        <Link
-          href="/onboarding"
-          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-        >
-          Go to Onboarding
-        </Link>
+          <Image source={icons.add} className="home-add-icon size-12" />
+        </View>
 
-        {/* Sign In */}
-        <Link
-          href="/(auth)/SignIn"
-          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-        >
-          Go to Sign In
-        </Link>
+        {/* Card Balance */}
+        <View className="home-balance-card my-2.5 min-h-50 justify-between gap-5 rounded-bl-4xl rounded-tr-4xl bg-accent p-6">
+          <Text className="home-balance-label text-xl font-sans-semibold text-white/80">
+            Balance
+          </Text>
+          <View className="home-balance-row flex-row items-center justify-between">
+            <Text className="home-balance-amount text-4xl font-sans-extrabold text-white">
+              {formatCurrency(HOME_BALANCE.amount)}
+            </Text>
+            <Text className="home-balance-date text-xl font-sans-medium text-white">
+              {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+            </Text>
+          </View>
+        </View>
 
-        {/* Sign Up */}
-        <Link
-          href="/(auth)/SignUp"
-          className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-        >
-          Go to Sign Up
-        </Link>
+        {/* */}
       </View>
     </SafeAreaView>
   );
